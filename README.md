2adef8d561fb68407d10fe70985deb32971ffa34
salva9315 <salva9315@gmail.com>
2024-07-20 13:32:40 +0200

    erlang_project_setup

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# chatserver_erlang
+chatserver
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ apps/chatserver/src/chatserver_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc chatserver public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(chatserver_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    chatserver_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
